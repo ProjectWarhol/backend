7ae458c27b2f6119ecccb2101aa04ba9a7cec237
osbadawy <omar.badawy@code.berlin>
2022-02-01 17:44:02 +0100

    dependensies added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      es2021: true,
-      node: true,
-    },
-    extends: ['airbnb-base', 'prettier'],
-    parserOptions: {
-      ecmaVersion: 12,
-    },
-    plugins: ['prefer-arrow'],
-    rules: {
-      'linebreak-style': ['error', 'unix'],
-      'prefer-arrow/prefer-arrow-functions': [
-        'error',
-        {
-          disallowPrototype: true,
-          singleReturnOnly: false,
-          classPropertiesAllowed: false,
-        },
-      ],
-      'prefer-arrow-callback': ['error', { allowNamedFunctions: false }],
-      'func-style': ['error', 'expression', { allowArrowFunctions: true }],
-    },
-  };
-  
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['prefer-arrow'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: false }],
+    'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+  },
+};
---
+++ .github/pull_request_template.md
@@ -0,0 +1,17 @@
+### Why are these changes introduced?
+
+...
+
+### What approach did you take?
+
+...
+
+### Links
+
+- [Ticket URL](url)
+
+### Checklists
+
+- [ ] All tests work (If your changes break a test, please update the test aswell)
+- [ ] No linting issues (eslint)
+- [ ] Code formatted correctly (prettier)
