facfb7f8b5f5b262a93eb0bde570bcbd70713bba
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-02-18 16:36:20 +0100

    ref: changed userId to id, using sessionObject

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const db = require('../models');
-const errHandler = require('../middlewares/error_handlers.middleware')
+const errHandler = require('../middlewares/error_handlers.middleware');
+const { sessionObject } = require('../util/sessionObject');
 const { generateToken } = require('../util/tokenGenerator');
 
 const {
@@ -121,24 +122,15 @@ exports.updatePassword = async (req, res, next) => {
 // Get User object from the username in the request
 exports.retrieveOne = async (req, res, next) => {
   const {
-    body: { userId },
+    body: { id },
   } = req;
 
-  User.findByPk(userId)
+  User.findByPk(id)
     .then((data) => {
       if (data) {
         res.status(200).send({
           message: 'User data sent successfully',
-          user: {
-            id: data.id,
-            userName: data.userName,
-            email: data.email,
-            avatar: data.avatar,
-            bio: data.bio,
-            promoters: data.promoters,
-            promoting: data.promoting,
-            verified: data.verified,
-          },
+          user: sessionObject(data),
         });
       } else {
         next(errHandler.noPathErrorHandler);
