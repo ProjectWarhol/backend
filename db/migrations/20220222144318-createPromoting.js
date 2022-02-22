ded50380ad956da19e9a91cfcd109cdab01b2c63
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-02-22 15:47:03 +0100

    feat: added promoting migration

---
+++ db/migrations/20220222144318-createPromoting.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+    );
+    await queryInterface.createTable('Promoting', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      userId: DataTypes.UUID,
+      promoterId: DataTypes.UUID,
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('UserWallet');
+  },
+};
