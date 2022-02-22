0708e7efd4664bcee5bf51b7e218121a4af18020
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-02-22 15:53:31 +0100

    feat: added promoting seeder

---
+++ db/seeders/20220222144751-promoting.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface) => {
+    const userIds = await queryInterface.sequelize.query(
+      `SELECT id from "User";`
+    );
+
+    await queryInterface.bulkInsert('Promoting', [
+      {
+        id: Sequelize.literal('uuid_generate_v4()'),
+        userId: userIds[0][0].id,
+        promoterId: userIds[0][1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: Sequelize.literal('uuid_generate_v4()'),
+        userId: userIds[0][0].id,
+        promoterId: userIds[0][2].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: Sequelize.literal('uuid_generate_v4()'),
+        userId: userIds[0][0].id,
+        promoterId: userIds[0][3].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: Sequelize.literal('uuid_generate_v4()'),
+        userId: userIds[0][1].id,
+        promoterId: userIds[0][0].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: Sequelize.literal('uuid_generate_v4()'),
+        userId: userIds[0][1].id,
+        promoterId: userIds[0][3].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: Sequelize.literal('uuid_generate_v4()'),
+        userId: userIds[0][3].id,
+        promoterId: userIds[0][2].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('Promoting', null, {});
+  },
+};
