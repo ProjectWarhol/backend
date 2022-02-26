5ee2379089db67f2c637d25aee6ee71dbe153ee1
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-02-26 15:34:29 +0100

    feat: added nftvote migration and seeder

---
+++ db/migrations/5-createNftVote.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+    );
+    await queryInterface.createTable('NftVote', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: Sequelize.DATE,
+      userId: {
+        type: DataTypes.UUID,
+        references: {
+          model: {
+            tableName: 'User',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      contentId: {
+        type: DataTypes.UUID,
+        references: {
+          model: {
+            tableName: 'NftContent',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('NftVote');
+  },
+};
