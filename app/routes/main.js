4f35114289b9e6d52df8c0a629dcd8bdd7389ef3
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-02-28 12:38:33 +0100

    feat: routing promoting requests to promoting.routes file

--- app/routes/main.js
+++ app/routes/main.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./user.routes');
+const promotingRoutes = require('./promoting.routes');
 
 // this file acts as the main router for all incoming requests
 router.get('/', (req, res) => {
@@ -10,5 +11,7 @@ router.get('/', (req, res) => {
 });
 
 router.use('/users', userRoutes);
+
+router.use('/promoting', promotingRoutes);
 
 module.exports = router;
