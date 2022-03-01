427f9d2d47fa828e9bc96b1d743d74e34fb27561
osbadawy <omar.badawy@code.berlin>
2022-03-01 17:40:31 +0100

    bugfix: deleted import of wallet routes

--- app/routes/main.js
+++ app/routes/main.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./user.routes');
-const walletRoutes = require('./wallet.routes');
 
 // this file acts as the main router for all incoming requests
 router.get('/', (req, res) => {
@@ -11,6 +10,5 @@ router.get('/', (req, res) => {
 });
 
 router.use('/users', userRoutes);
-router.use('/wallet', walletRoutes);
 
 module.exports = router;
