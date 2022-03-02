ce3c7c973ff4b5dc7c56fbe803853041531b770d
osbadawy <omar.badawy@code.berlin>
2022-03-02 11:57:48 +0100

    update: session cookie is cleared from header

--- app/controllers/session.controller.js
+++ app/controllers/session.controller.js
@@ -58,6 +58,7 @@ exports.validateSession = (req, res, next) => {
       user: currentUser,
     });
   }
+  res.status(200).clearCookie('my.sid', { path: '/' });
   req.session.destroy();
   const error = new Error('Unauthorized please Login');
   error.status = 401;
