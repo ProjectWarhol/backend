78dfc23796903adaa06448b92528f4fdf16344fb
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-04 18:46:22 +0100

    feat: added createUserPromotes controller

--- app/controllers/promoting.controller.js
+++ app/controllers/promoting.controller.js
@@ -5,6 +5,7 @@ const { sessionObject } = require('../util/sessionObject');
 const {
   Promoting,
   User,
+  Sequelize: { Op },
 } = db;
 
 // Get all users that a user with promoterId promotes
@@ -30,4 +31,52 @@ exports.userPromotes = (req, res, next) => {
     .catch((err) => {
       next(errHandler.defaultErrorHandler(err));
     });
-}
+};
+
+exports.createUserPromotes = (req, res, next) => {
+  const {
+    body: { promoterId, userId },
+  } = req;
+
+  Promoting.findOrCreate({
+    where: {
+      [Op.and]: [
+        { promoterId }, 
+        { userId }
+      ],
+    },
+    defaults: {
+      ...{ promoterId },
+      ...{ userId },
+    },
+  })
+    // eslint-disable-next-line no-unused-vars
+    .then(([newPromoting, created]) => {
+      if (created) {
+        User.increment('promoting', {
+          where: {
+            id: promoterId,
+          },
+        })
+          .then(() => {
+            User.increment('promoters', {
+              where: {
+                id: userId,
+              },
+            });
+          })
+            .then(() => {
+              res.status(200).send({
+                message: 'Promotion added successfully',
+              });
+            });
+      } else {
+        res.status(409).send({
+          message: 'Already promoting',
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
