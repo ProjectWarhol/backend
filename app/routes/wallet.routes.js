a3fece557c32fddf5f333e1bb0ccad706a2bdfc1
osbadawy <omar.badawy@code.berlin>
2022-03-07 12:26:28 +0100

    feat: added controller to route

--- app/routes/wallet.routes.js
+++ app/routes/wallet.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const wallet = require('../controllers/wallet.controller');
 
 const router = express.Router();
 
 // Post Wallet request
-router.post('/:id');
+router.post('/:id', wallet.createWAllet);
 
 module.exports = router;
