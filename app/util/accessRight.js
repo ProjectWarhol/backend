cfa89f275273b595e03b79534a0c85559c59dc70
osbadawy <omar.badawy@code.berlin>
2022-03-07 14:36:31 +0100

    ref: refactored helper function and createWallet

--- app/util/accessRight.js
+++ app/util/accessRight.js
@@ -4,12 +4,18 @@ const db = require('../models');
 const { User } = db;
 
 exports.checkIfUserHasWallet = (id, next) => {
-  User.findOne({ id }, (_err, user) => {
-    if (!user.walletId) {
-      return true;
-    }
-    return false;
-  }).catch((err) => {
-    next(errHandler.defaultErrorHandler(err));
-  });
+  let result;
+
+  User.findByPk(id)
+    .then((user) => {
+      if (!user.walletId) {
+        result = true;
+      }
+      result = false;
+    })
+    .catch((err) => {
+      next(errHandler.defaultErrorHandler(err));
+    });
+
+  return result;
 };
