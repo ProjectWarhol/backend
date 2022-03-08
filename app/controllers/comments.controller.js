67145c2ba0b39d05845e5622cb3f6fca43838c66
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-08 19:10:26 +0100

    feat: added retrieveComments function

--- app/controllers/comments.controller.js
+++ app/controllers/comments.controller.js
@@ -1,19 +1,60 @@
 const { Sequelize } = require('../models');
 const db = require('../models');
+const { commentObject } = require('../util/commentObject');
 
 const {
   Sequelize: { Op },
+  NftContent,
+  Comments,
+  User,
 } = db;
 
-// Get all users that a user with promoterId promotes
-exports.retrieveComments = (req, res, next) => {
-  const {
-    body: { id },
-  } = req;
-
+const defaultCommentsError = (err) => {
+  const error = new Error('Something went wrong');
+  error.status = 500;
+  if (err.name !== 'SequelizeDatabaseError') {
+    error.err = err;
+  }
+  return error;
 };
 
-// Get all users that a user with promoterId promotes
+// Retrieve comments on picture
+exports.retrieveComments = (req, res, next) => {
+  const {
+    body: { id, offset },
+  } = req;
+
+  Comments.findAll({
+    ...{ offset },
+    limit: 20,
+    include: [
+      {
+        model: NftContent,
+        attributes: [],
+        required: true,
+        where: { id },
+      },
+      {
+        model: User,
+        on: {
+          id: { [Op.eq]: Sequelize.col('Comments.userId') },
+        },
+      },
+    ]
+  })
+    .then((data) => {
+      const commentObjects = data.map(comment => commentObject(comment));
+      res.status(200).send({
+        message: 'Comments sent successfully',
+        data: commentObjects,
+      });
+    })
+    .catch((err) => {
+      next(defaultCommentsError(err));
+    });
+};
+
+// Post a comment on a picture
 exports.createComment = (req, res, next) => {
   const {
     body: { id },
