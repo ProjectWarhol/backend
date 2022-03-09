28c6de69b68c59180abc5928f410c4023033f262
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-09 14:56:50 +0100

    update: commentObject extracts updatedAt instead of createdAt

--- app/util/commentObject.js
+++ app/util/commentObject.js
@@ -2,5 +2,5 @@ exports.commentObject = (comment) => ({
   userName: comment.User.userName,
   avatar: comment.User.avatar,
   comment: comment.comment,
-  createdAt: comment.createdAt,
+  updatedAt: comment.updatedAt,
 });
