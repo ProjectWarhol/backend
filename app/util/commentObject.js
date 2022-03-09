d22418a35d89d9fbc8eefc8070cc53a8c8f8bb04
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-09 22:33:47 +0100

    update: sending updatedAt too

--- app/util/commentObject.js
+++ app/util/commentObject.js
@@ -2,5 +2,6 @@ exports.commentObject = (comment) => ({
   userName: comment.User.userName,
   avatar: comment.User.avatar,
   comment: comment.comment,
+  createdAt: comment.createdAt,
   updatedAt: comment.updatedAt,
 });
