0f9ea93fa5966b03e68e81f0ca5320ff6e1ad888
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-10 14:00:01 +0100

    Change path to openzeppelin

--- app/blockchain/contracts/NftMinting.sol
+++ app/blockchain/contracts/NftMinting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NftMinting is ERC721 {
   uint256 public tokenCounter;
@@ -9,11 +9,11 @@ contract NftMinting is ERC721 {
     tokenCounter = 0;
   }
 
-  function safeMint(string memory tokenURI) returns (uint256) {
-    uint256 newTokenId = tokenCounter;
-    _safeMint(msg.sender, tokenURI);
-    _setTokenURI(newTokenId, tokenURI);
-    tokenCounter += 1;
-    return newTokenId;
-  }
+  // function safeMint(string memory tokenURI) public returns (uint256) {
+  //   uint256 newTokenId = tokenCounter;
+  //   _safeMint(msg.sender, tokenURI);
+  //   _setTokenURI(newTokenId, tokenURI);
+  //   tokenCounter += 1;
+  //   return newTokenId;
+  // }
 }
