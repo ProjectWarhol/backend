3f8e9623c4fcfdb834790dab88974034cd3e2f5f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-10 09:56:53 +0100

    Add a tokenCounter type

--- app/blockchain/contracts/NftMinting.sol
+++ app/blockchain/contracts/NftMinting.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NftMinting is ERC721 {
+  uint256 public tokenCounter;
   constructor() public ERC721 ("WarholToken","WT") {
-    
+    tokenCounter = 0;
   }
 }
