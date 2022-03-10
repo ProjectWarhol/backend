d219a838223db81a6b4ce0f27c96f5a9310da13a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-10 14:32:48 +0100

    Test a name and a symbol for minting contract

--- test/app/blockchain/contracts/NftMinting.test.js
+++ test/app/blockchain/contracts/NftMinting.test.js
@@ -10,14 +10,24 @@ require('chai')
 
 contract('NftMinting', ()=> {
   let contract
+  before(async()=> {
+    contract = await NftMinting.deployed()
+  })
   describe('deployment', async()=> {
     it('deploys successfully', async() => {
-      contract = await NftMinting.deployed()
       const {address} = contract
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
       assert.notEqual(address, null)
       assert.notEqual(address, undefined)
+    })
+    it('has a name', async()=> {
+      const name = await contract.name()
+      assert.equal(name, 'WarholToken')
+    })
+    it('has a symbol', async()=> {
+      const symbol = await contract.symbol()
+      assert.equal(symbol, 'WT')
     })
   })
 })
