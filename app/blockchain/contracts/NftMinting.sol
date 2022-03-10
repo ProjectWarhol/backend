125e6a0f724855dae7afcb449c3e8ab6b2432ef6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-10 10:37:30 +0100

    Write safeMint func

--- app/blockchain/contracts/NftMinting.sol
+++ app/blockchain/contracts/NftMinting.sol
@@ -5,7 +5,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NftMinting is ERC721 {
   uint256 public tokenCounter;
-  constructor() public ERC721 ("WarholToken","WT") {
+  constructor() ERC721 ("WarholToken","WT") {
     tokenCounter = 0;
+  }
+
+  function safeMint(string memory tokenURI) returns (uint256) {
+    uint256 newTokenId = tokenCounter;
+    _safeMint(msg.sender, tokenURI);
+    _setTokenURI(newTokenId, tokenURI);
+    tokenCounter += 1;
+    return newTokenId;
   }
 }
