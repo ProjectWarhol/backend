41a45209e191a326e8104f9243c0654de4369c4d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-10 09:51:56 +0100

    Add args to the constructor

--- app/blockchain/contracts/NftMinting.sol
+++ app/blockchain/contracts/NftMinting.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NftMinting is ERC721 {
-  
+  constructor() public ERC721 ("WarholToken","WT") {
+    
+  }
 }
