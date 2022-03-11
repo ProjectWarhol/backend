13186d9301636f366a0c6a3b3b5792668dc13602
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-11 09:35:10 +0100

    Add _safeMint and _setTokenURI

--- app/blockchain/contracts/NftMinting.sol
+++ app/blockchain/contracts/NftMinting.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.8.12;
 
 import "../../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../../../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+import "../../../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NftMinting is ERC721, AccessControl {
-  
+contract NftMinting is ERC721, AccessControl, ERC721URIStorage {
   bytes32 public constant MINTER = keccak256("MINTER");
+  uint public tokenCounter;
   constructor() ERC721 ("WarholToken","WT")  {
     _setupRole(MINTER, msg.sender);
   }
 
   function mint(string memory _tokenURI) public onlyRole(MINTER){
+    uint256 _id = tokenCounter;
+    _safeMint(msg.sender, _id);
+    // _setTokenURI is removed from prigma 0.8.0
+    _setTokenURI(_id, _tokenURI);
+    tokenCounter = tokenCounter + 1;
   }
 }
