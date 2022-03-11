56d45b08b9586d2e1950b384c0decd940c22fac6
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-11 15:25:37 +0100

    feat: implemented userHasWallet

--- app/middlewares/verification.middleware.js
+++ app/middlewares/verification.middleware.js
@@ -1,10 +1,22 @@
 const { checkIfUserHasWallet } = require('../util/accessRight');
 
-exports.validateWalletPosession = async (req, res, next) => {
-  const { id } = req.body;
+exports.userHasNotWallet = async (req, res, next) => {
+  const { id } = req.session.user;
   const hasWallet = await checkIfUserHasWallet(id, res);
 
-  if (hasWallet !== false) {
+  if (hasWallet === true) {
+    const error = new Error('Forbidden');
+    error.status = 403;
+    return next(error);
+  }
+  return next();
+};
+
+exports.userHasWallet = async (req, res, next) => {
+  const { id } = req.session.user;
+  const hasWallet = await checkIfUserHasWallet(id, res);
+
+  if (hasWallet === false) {
     const error = new Error('Forbidden');
     error.status = 403;
     return next(error);
