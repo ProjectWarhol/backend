ce6deccce4ea4bb92135e7606df042cc80b00e33
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-13 10:34:47 +0100

    Write tests for safeMint logs

--- test/app/blockchain/contracts/NftMinting.test.js
+++ test/app/blockchain/contracts/NftMinting.test.js
@@ -1,38 +1,46 @@
 // eslint-disable-next-line
-const NftMinting = artifacts.require('./NftMinting')
+const NftMinting = artifacts.require('./NftMinting');
 /* eslint-disable no-undef */
 // third-party
 const { assert } = require('chai');
 
-require('chai')
-  .use(require('chai-as-promised'))
-  .should()
+require('chai').use(require('chai-as-promised')).should();
 
-contract('NftMinting', ()=> {
-  let contract
-  before(async()=> {
-    contract = await NftMinting.deployed()
-  })
-  describe('deployment', async()=> {
-    it('deploys successfully', async() => {
-      const {address} = contract
-      assert.notEqual(address, 0x0)
-      assert.notEqual(address, '')
-      assert.notEqual(address, null)
-      assert.notEqual(address, undefined)
-    })
-    it('has a name', async()=> {
-      const name = await contract.name()
-      assert.equal(name, 'WarholToken')
-    })
-    it('has a symbol', async()=> {
-      const symbol = await contract.symbol()
-      assert.equal(symbol, 'WT')
-    }) 
-    it('safe minting', async()=> {
-      await contract.safeMint('https://storage.googleapis.com/opensea-prod.appspot.com/creature/1.png')
-      const tokenCounter = await contract.tokenCounter()
-      assert.equal(tokenCounter, 1)
-    }) 
-  })
-})
+contract('NftMinting', () => {
+  let contract;
+  before(async () => {
+    contract = await NftMinting.deployed();
+  });
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const { address } = contract;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, '');
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+    it('has a name', async () => {
+      const name = await contract.name();
+      assert.equal(name, 'WarholToken');
+    });
+    it('has a symbol', async () => {
+      const symbol = await contract.symbol();
+      assert.equal(symbol, 'WT');
+    });
+    it('safe minting', async () => {
+      const result = await contract.safeMint(
+        'https://storage.googleapis.com/opensea-prod.appspot.com/creature/1.png'
+      );
+      const tokenCounter = await contract.tokenCounter();
+      const event = result.logs[0].args;
+      // SUCCESS
+      assert.equal(tokenCounter, 1);
+      assert.equal(event.tokenId.toNumber(), 0, 'id is correct');
+      assert.equal(
+        event.from,
+        '0x0000000000000000000000000000000000000000',
+        'from is correct'
+      );
+    });
+  });
+});
