7f5de23b87dbdd417a6fe4edabfdc976cae79a01
osbadawy <omar.badawy@code.berlin>
2022-03-16 23:51:05 +0100

    feat: added delete routes

--- app/routes/wallet.routes.js
+++ app/routes/wallet.routes.js
@@ -4,15 +4,16 @@ const router = express.Router();
 const {
   storePrivateKey,
   createWallet,
+  deleteWallet,
 } = require('../controllers/wallet.controller');
-const {
-  userHasNotWallet,
-} = require('../middlewares/verification.middleware');
+const { userHasNotWallet } = require('../middlewares/verification.middleware');
 
 // Post Wallet request
 router.post('/createWallet', userHasNotWallet, createWallet);
 
 // Post PrivateKey
 router.post('/:id', storePrivateKey);
+
+router.delete('/id', deleteWallet);
 
 module.exports = router;
