f3e09ba56793b47305e07ba93f1b598deb556f49
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-17 18:34:07 +0100

    update: checkUserIdentity now checks for userId in body

--- app/middlewares/authentication.middleware.js
+++ app/middlewares/authentication.middleware.js
@@ -1,5 +1,5 @@
 exports.checkUserIdentity = (req, res, next) => {
-  if (req.session.user.id !== req.body.id) {
+  if (req.session.user.id !== req.body.userId) {
     const error = new Error('Unauthorized');
     error.status = 401;
     return next(error);
