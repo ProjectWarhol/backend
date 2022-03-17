f14cad821a3d85ff77238b808dbf92b2b23dd672
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-17 18:34:26 +0100

    update: applied checkUserIdentity to some routes

--- app/routes/comments.routes.js
+++ app/routes/comments.routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const comments = require('../controllers/comments.controller');
+const { checkUserIdentity } = require('../middlewares/authentication.middleware');
 const { isLoggedIn } = require('../middlewares/authorization.middleware');
 
 // Fetch comments on a picture
 router.get('/', isLoggedIn, comments.retrieveComments);
 
 // Create comment on a picture
-router.post('/', isLoggedIn, comments.createComment);
+router.post('/', isLoggedIn, checkUserIdentity, comments.createComment);
 
 module.exports = router;
--- app/routes/promoting.routes.js
+++ app/routes/promoting.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const promoting = require('../controllers/promoting.controller');
+const { checkUserIdentity } = require('../middlewares/authentication.middleware');
 const { isLoggedIn } = require('../middlewares/authorization.middleware');
 
 // Get user promoters
@@ -11,9 +12,9 @@ router.get('/isPromoted', isLoggedIn, promoting.userIsPromoted);
 router.get('/', isLoggedIn, promoting.userPromoting);
 
 // Post user promotes
-router.post('/:promotedId', isLoggedIn, promoting.promotingOneUser);
+router.post('/:promotedId', isLoggedIn, checkUserIdentity, promoting.promotingOneUser);
 
 // Delete user promotes
-router.delete('/:promotedId', isLoggedIn, promoting.unpromotingOneUser);
+router.delete('/:promotedId', isLoggedIn, checkUserIdentity, promoting.unpromotingOneUser);
 
 module.exports = router;
