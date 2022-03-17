bced40645e5db02606531ef00a8aa177ceba7a8b
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-17 18:15:11 +0100

    ref: moved userIsPromoted route to new file

--- app/routes/promoting.routes.js
+++ app/routes/promoting.routes.js
@@ -4,11 +4,8 @@ const router = express.Router();
 const promoting = require('../controllers/promoting.controller');
 const { isLoggedIn } = require('../middlewares/authorization.middleware');
 
-// Get user promoters
-router.get('/isPromoted', isLoggedIn, promoting.userIsPromoted);
-
 // Get users promoted by user
-router.get('/', isLoggedIn, promoting.userPromoting);
+router.get('/:userId', isLoggedIn, promoting.userPromoting);
 
 // Post user promotes
 router.post('/:promotedId', isLoggedIn, promoting.promotingOneUser);
