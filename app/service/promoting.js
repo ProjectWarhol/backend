9b3c079d2013dbb047d08a7c7c05a1a8374ff7f6
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-03-21 23:14:39 +0100

    ref: removed increment and decrement functions

--- app/service/promoting.js
+++ app/service/promoting.js
@@ -9,52 +9,6 @@ const {
 
 const { User, Promoting } = db;
 
-exports.incrementPromoting = async (userId, promotedId, res) => {
-  const updatedUsers = {};
-
-  try {
-    updatedUsers.promoter = await User.increment('promoting', {
-      where: {
-        id: userId,
-      },
-    });
-
-    updatedUsers.promoted = await User.increment('promoters', {
-      where: {
-        id: promotedId,
-      },
-    });
-
-    updatedUsers.promoter = sessionObject(updatedUsers.promoter[0][0][0]);
-    updatedUsers.promoted = sessionObject(updatedUsers.promoted[0][0][0]);
-  } catch (err) {
-    defaultErrorHandler(res, 'Something went wrong while updating the users');
-    return null;
-  }
-
-  return updatedUsers;
-};
-
-exports.decrementPromoting = async (userId, promotedId) => {
-  const updatedUsers = {};
-
-  updatedUsers.promoter = await User.decrement('promoting', {
-    where: {
-      id: userId,
-    },
-  });
-
-  updatedUsers.promoted = await User.decrement('promoters', {
-    where: {
-      id: promotedId,
-    },
-  });
-
-  updatedUsers.promoter = sessionObject(updatedUsers.promoter[0][0][0]);
-  updatedUsers.promoted = sessionObject(updatedUsers.promoted[0][0][0]);
-  return updatedUsers;
-};
-
 exports.getPromotions = async (user, res) => {
   const promotions = await user
     .getUserPromotions({
