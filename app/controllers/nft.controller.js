e1f011916c02b44d767b3646dc6da81ff7a2f940
Maurice <maurice.gerhardt@code.berlin>
2022-03-21 15:49:11 +0100

    feat: make files uploadeable
    - delete them from temp folder after routes ended

--- app/controllers/nft.controller.js
+++ app/controllers/nft.controller.js
@@ -1,6 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+
+const removeTemporaryFile = (filePath) => {
+	try {
+		fs.unlinkSync(filePath);
+		return true;
+	} catch (err) {
+		console.error(err);
+		return false;
+	}
+}
+
 // Mint NFT
-exports.mintNft = (req, res, next) => {
-	res.status(200).send({
-		message: 'Successfully minted NFT'
+exports.mintNft = async (req, res) => {
+	if (!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+	const file = req.files.nftSource;
+	const filePath = path.join(__dirname, '/tmp_nft_sources/', file.name);
+	file.mv(filePath, (err) => {
+		if (err) {
+      return res.status(500).send(err);
+    }
+		res.status(200).send('NFT minted successfully')
+		removeTemporaryFile(filePath);
 	});
 };
