eb489ba3cc7266862c8c35c4339371fee7a799f2
osbadawy <omar.badawy@code.berlin>
2022-04-05 15:42:02 +0200

    update: removal of wallet routes

--- app/routes/wallet.routes.js
+++ app/routes/wallet.routes.js
@@ -3,21 +3,13 @@ const express = require('express');
 const router = express.Router();
 const {
   retrieveWallet,
-  storePrivateKey,
-  createWallet,
   deleteWallet,
 } = require('../controllers/wallet.controller');
-const { userHasNotWallet } = require('../middlewares/verification.middleware');
-
-// Post Wallet request
-router.post('/createWallet', userHasNotWallet, createWallet);
 
 // Get wallet request
 router.get('/:id', retrieveWallet);
 
-// Post PrivateKey
-router.post('/:id', storePrivateKey);
-
+// Delete wallet request
 router.delete('/:id', deleteWallet);
 
 module.exports = router;
