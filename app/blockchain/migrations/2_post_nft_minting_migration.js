0aa70e9f92de8aaa0aad22aea9540d2cee2e05d6
Maurice <maurice.gerhardt@code.berlin>
2022-04-08 11:30:41 +0200

    fix: remove unnecessary console.log

--- app/blockchain/migrations/2_post_nft_minting_migration.js
+++ app/blockchain/migrations/2_post_nft_minting_migration.js
@@ -4,5 +4,4 @@ const PostNftMinting = artifacts.require('PostNftMinting');
 module.exports = async function (deployer) {
   await deployer.deploy(PostNftMinting);
   const instance = await PostNftMinting.deployed();
-  console.log('ADDRESS:', instance.address);
 };
