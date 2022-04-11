ee9d80070005b3c6170ae872a8cf508f91855c30
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-11 10:47:46 +0200

    ref: cleaned up and added validations to Comments

---
+++ db/migrations/4-createComments.js
@@ -0,0 +1,65 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+    );
+    await queryInterface.createTable('Comments', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        primaryKey: true,
+        allowNull: false,
+      },
+      comment: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        references: {
+          model: {
+            tableName: 'User',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        validate: {
+          isUUID: 4,
+        },
+      },
+      contentId: {
+        type: DataTypes.UUID,
+        references: {
+          model: {
+            tableName: 'NftContent',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        validate: {
+          isUUID: 4,
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Comments');
+  },
+};
