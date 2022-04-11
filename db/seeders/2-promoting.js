35cd38eb35debcb9a2843245007d5cf584bef287
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-11 18:37:14 +0200

    ref: cleaned up and added validations to Promoting

--- db/seeders/2-promoting.js
+++ db/seeders/2-promoting.js
@@ -8,80 +8,28 @@ module.exports = {
       {
         userId: userIds[0][0].id,
         promotedId: userIds[0][1].id,
-        createdAt: new Date(),
       },
       {
         userId: userIds[0][0].id,
         promotedId: userIds[0][2].id,
-        createdAt: new Date(),
       },
       {
         userId: userIds[0][0].id,
         promotedId: userIds[0][3].id,
-        createdAt: new Date(),
       },
       {
         userId: userIds[0][1].id,
         promotedId: userIds[0][0].id,
-        createdAt: new Date(),
       },
       {
         userId: userIds[0][1].id,
         promotedId: userIds[0][3].id,
-        createdAt: new Date(),
       },
       {
         userId: userIds[0][3].id,
         promotedId: userIds[0][2].id,
-        createdAt: new Date(),
       },
     ]);
-    await queryInterface.sequelize.query(
-      `CREATE OR REPLACE FUNCTION incrementPromoting() 
-      RETURNS TRIGGER 
-      LANGUAGE PLPGSQL 
-      AS 
-      $$ 
-      BEGIN 
-      UPDATE "User" 
-      SET "promoting"="promoting"+1 
-      WHERE "id"=NEW."userId"; 
-      UPDATE "User" 
-      SET "promoters"="promoters"+1 
-      WHERE "id"=NEW."promotedId"; 
-      RETURN NEW; 
-      END; 
-      $$;`
-    );
-    await queryInterface.sequelize.query(
-      `CREATE OR REPLACE FUNCTION decrementPromoting() 
-      RETURNS TRIGGER 
-      LANGUAGE PLPGSQL 
-      AS 
-      $$ 
-      BEGIN 
-      UPDATE "User" 
-      SET "promoting"="promoting"-1 
-      WHERE "id"=OLD."userId"; 
-      UPDATE "User" 
-      SET "promoters"="promoters"-1 
-      WHERE "id"=OLD."promotedId"; 
-      RETURN OLD; 
-      END; 
-      $$;`
-    );
-    await queryInterface.sequelize.query(
-      `CREATE TRIGGER createPromoting 
-      AFTER INSERT ON "Promoting" 
-      FOR EACH ROW 
-      EXECUTE PROCEDURE incrementPromoting();`
-    );
-    await queryInterface.sequelize.query(
-      `CREATE TRIGGER deletePromoting 
-      AFTER DELETE ON "Promoting" 
-      FOR EACH ROW 
-      EXECUTE PROCEDURE decrementPromoting();`
-    );
   },
 
   down: async (queryInterface) => {
