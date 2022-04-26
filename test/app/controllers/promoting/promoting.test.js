4740318b22dccf83f7955aa6875dd65bb4f75a36
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-04-26 17:02:11 +0200

    finished promoting tests

--- test/app/controllers/promoting/promoting.test.js
+++ test/app/controllers/promoting/promoting.test.js
@@ -9,20 +9,13 @@ const { getAgent } = require('../../../1session.test')
 chai.use(chaiHttp);
 require('dotenv').config();
 
-let userId;
-
 const agent = getAgent();
 
 describe('GET /promoting', () => {
 
-  before(async () => {
-    userId = await sequelize.getQueryInterface().sequelize.query(`SELECT "id" FROM "User" WHERE "email"='${process.env.TEST_EMAIL}'`);
-    userId = userId[0][0].id;
-  });
-
   it('should return a list of promoted users', (done) => {
     agent
-      .get(`/promoting/${userId}`)
+      .get(`/promoting/${agent.id}`)
       .send()
       .end((err, res) => {
         // eslint-disable-next-line no-unused-expressions
@@ -37,7 +30,7 @@ describe('GET /promoting', () => {
   it('should drop request if user is not logged in', (done) => {
     chai
       .request(app)
-      .get(`/promoting/${userId}`)
+      .get(`/promoting/${agent.id}`)
       .send()
       .end((err, res) => {
         // eslint-disable-next-line no-unused-expressions
@@ -66,7 +59,7 @@ describe('GET /promoted', () => {
 
   it('should return a list of users promoting', (done) => {
     agent
-      .get(`/promoted/${userId}`)
+      .get(`/promoted/${agent.id}`)
       .send()
       .end((err, res) => {
         // eslint-disable-next-line no-unused-expressions
@@ -81,7 +74,7 @@ describe('GET /promoted', () => {
   it('should drop request if user is not logged in', (done) => {
     chai
       .request(app)
-      .get(`/promoted/${userId}`)
+      .get(`/promoted/${agent.id}`)
       .send()
       .end((err, res) => {
         // eslint-disable-next-line no-unused-expressions
@@ -107,5 +100,108 @@ describe('GET /promoted', () => {
 });
 
 describe('POST /promoting', () => {
+  let promotedId;
 
+  before(async () => {
+    await sequelize.getQueryInterface().sequelize.query(`DELETE FROM "Promoting" WHERE "userId"='${agent.id}'`);
+    promotedId = await sequelize.getQueryInterface().sequelize.query(`SELECT "id" FROM "User" WHERE "id"!='${agent.id}' LIMIT 1;`);
+    promotedId = promotedId[0][0].id;
+  });
+
+  it('should create a promotion successfully', (done) => {
+    agent
+      .post(`/promoting/${promotedId}`)
+      .send({
+        userId: agent.id,
+      })
+      .end((err, res) => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message', 'Promotion added successfully');
+        done();
+      })
+  });
+
+  it('should not allow double entries', (done) => {
+    agent
+      .post(`/promoting/${promotedId}`)
+      .send({
+        userId: agent.id,
+      })
+      .end((err, res) => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(err).to.be.null;
+        expect(res).to.have.status(409);
+        expect(res.body).to.have.nested.property('error.message', 'Conflict: Promotion already exists');
+        done();
+      })
+  });
+
+  it('should not create self-promotion', (done) => {
+    agent
+      .post(`/promoting/${agent.id}`)
+      .send({
+        userId: agent.id,
+      })
+      .end((err, res) => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(err).to.be.null;
+        expect(res).to.have.status(409);
+        expect(res.body).to.have.nested.property('error.message', 'Conflict: Self-promotion');
+        done();
+      })
+  });
+
+  it('should drop request if user is not logged in', (done) => {
+    chai
+      .request(app)
+      .post(`/promoting/${agent.id}`)
+      .send()
+      .end((err, res) => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(err).to.be.null;
+        expect(res).to.have.status(403);
+        done();
+      })
+  });
+});
+
+describe('DELETE /promoting', () => {
+  let promotedId;
+
+  before(async () => {
+    promotedId = await sequelize.getQueryInterface().sequelize.query(`SELECT "promotedId" FROM "Promoting" WHERE "userId"='${agent.id}' LIMIT 1;`);
+    promotedId = promotedId[0][0].promotedId;
+  });
+
+  it('should delete a promotion successfully', (done) => {
+    agent
+      .delete(`/promoting/${promotedId}`)
+      .send({
+        userId: agent.id,
+      })
+      .end((err, res) => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message', 'Promotion deleted successfully');
+        done();
+      })
+  });
+
+  it('should 404 if promotion is not found', (done) => {
+    agent
+      .delete(`/promoting/${agent.id}`)
+      .send({
+        userId: agent.id,
+      })
+      .end((err, res) => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(err).to.be.null;
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.nested.property('error.message', 'Promotion Not found');
+        done();
+      })
+  });
 });
