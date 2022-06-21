152f7ac5607685bcd6ca792d2fc2477d0ef2d2df
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-06-21 18:17:08 +0200

    update: updated verification middlewares and accessRight utility functions error handling

--- app/middlewares/verification.middleware.js
+++ app/middlewares/verification.middleware.js
@@ -1,26 +1,33 @@
 const { checkIfUserHasWallet } = require('../util/accessRight');
 
-exports.userHasNotWallet = async (req, res, next) => {
+exports.userHasNotWallet = async (req, _res, next) => {
   const { id } = req.body;
-  const hasWallet = await checkIfUserHasWallet(id, res);
 
-  if (hasWallet === true) {
-    const error = new Error('Forbidden');
-    error.status = 403;
-    return next(error);
+  let hasWallet;
+  try {
+    hasWallet = await checkIfUserHasWallet(id);
+  } catch (err) {
+    return next(err);
   }
 
+  if (hasWallet) {
+    return next(new StatusError('User already has a wallet', 403));
+  }
   return next();
 };
 
 exports.userHasWallet = async (req, res, next) => {
   const { id } = req.body;
-  const hasWallet = await checkIfUserHasWallet(id, res);
 
-  if (hasWallet === false) {
-    const error = new Error('Forbidden');
-    error.status = 403;
-    return next(error);
+  let hasWallet;
+  try {
+    hasWallet = await checkIfUserHasWallet(id);
+  } catch (err) {
+    return next(err);
+  }
+
+  if (!hasWallet) {
+    return next(new StatusError('User has no wallet', 403));
   }
   return next();
 };
