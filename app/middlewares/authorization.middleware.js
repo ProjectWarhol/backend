523c123ea26f16d456624bcd44c504be5b58f80e
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-06-21 17:16:30 +0200

    update: updated authorization middlewares error handling

--- app/middlewares/authorization.middleware.js
+++ app/middlewares/authorization.middleware.js
@@ -1,21 +1,15 @@
 /* eslint-disable consistent-return */
 const { body, validationResult } = require('express-validator');
-const {
-  unauthorizedHandler,
-  defaultWrongInputHandler,
-  defaultErrorHandler,
-} = require('./error_handlers.middleware');
 
 // checks if user is logged in
-exports.isLoggedIn = (req, res, next) => {
+exports.isLoggedIn = (req, _res, next) => {
   if (!req.session.user) {
-    unauthorizedHandler(res, 'user not logged in');
-    return;
+    return next(new StatusError('User not logged in', 403));
   }
   return next();
 };
 
-exports.checkLoginInput = async (req, res, next) => {
+exports.checkLoginInput = async (req, _res, next) => {
   await body('userCredential')
     .isEmail()
     .withMessage('userName')
@@ -24,12 +18,12 @@ exports.checkLoginInput = async (req, res, next) => {
     .run(req);
 
   if (validationResult(req).errors.length === 2) {
-    return defaultWrongInputHandler(res, 'Invalid username or email');
+    return next(new StatusError('Invalid username or email', 422));
   }
   if (validationResult(req).errors.length === 1) {
     req.body.type = validationResult(req).errors[0].msg;
     return next();
   }
 
-  return defaultErrorHandler(res, 'Something went wrong');
+  return next(new StatusError('Something went wrong', 500));
 };
