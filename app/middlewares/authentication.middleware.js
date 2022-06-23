58322b3fb424a8abf54c5c143631f27e1715cbc0
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-06-23 13:43:24 +0200

    update: fixing verbose exception

--- app/middlewares/authentication.middleware.js
+++ app/middlewares/authentication.middleware.js
@@ -1,6 +1,6 @@
 exports.checkUserIdentity = (req, _res, next) => {
   if (req.session.user.id !== req.body.userId) {
-    return next(new StatusError('Invalid userId', 401));
+    return next(new StatusError('unauthorized', 401));
   }
   return next();
 };
