9b6012ea19f7507520099565057f0695d3d98570
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-09-05 13:55:36 +0200

    update: added checkUserIdentity to updatePassword route

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -13,6 +13,7 @@ const {
   storePrivateKey,
 } = require('../controllers/wallet.controller');
 const { userHasNotWallet } = require('../middlewares/verification.middleware');
+const { checkUserIdentity } = require('../middlewares/authentication.middleware');
 
 // Post login request
 router.post('/login', checkLoginInput, session.login);
@@ -27,7 +28,7 @@ router.get('/session', session.validateSession);
 router.get('/:userName', isLoggedIn, user.retrieveOne);
 
 // patch User Password
-router.patch('/updatePassword', isLoggedIn, user.updatePassword);
+router.patch('/updatePassword', isLoggedIn, checkUserIdentity, user.updatePassword);
 
 // Update a User with id
 router.patch('/:id', isLoggedIn, user.updateOne);
