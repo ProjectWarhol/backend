2675a2e83db7e65cfcff8856b36d507056d2c864
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-09-05 12:04:17 +0200

    update: promosified checkLoginInput middleware

--- app/middlewares/authorization.middleware.js
+++ app/middlewares/authorization.middleware.js
@@ -9,21 +9,22 @@ exports.isLoggedIn = (req, _res, next) => {
   return next();
 };
 
-exports.checkLoginInput = async (req, _res, next) => {
-  await body('userCredential')
+exports.checkLoginInput = (req, _res, next) => {
+  body('userCredential')
     .isEmail()
     .withMessage('userName')
     .matches(/^[a-z0-9-_.]{4,20}$/i)
     .withMessage('email')
-    .run(req);
+    .run(req)
+      .then(() => {
+        if (validationResult(req).errors.length === 2) {
+          return next(new StatusError('Invalid username or email', 422));
+        }
+        if (validationResult(req).errors.length === 1) {
+          req.body.type = validationResult(req).errors[0].msg;
+          return next();
+        }
 
-  if (validationResult(req).errors.length === 2) {
-    return next(new StatusError('Invalid username or email', 422));
-  }
-  if (validationResult(req).errors.length === 1) {
-    req.body.type = validationResult(req).errors[0].msg;
-    return next();
-  }
-
-  return next(new StatusError('Something went wrong', 500));
+        return next(new StatusError('Something went wrong', 500));
+      })
 };
