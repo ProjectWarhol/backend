1bde3571b1f04a26d99af1ea0011f3c5120a4a1d
julklingel <julklingel@gmail.com>
2022-10-07 16:12:04 +0200

    fix/route-added-checkUserIdentity-middleware

--- app/routes/company.routes.js
+++ app/routes/company.routes.js
@@ -2,18 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 const company = require('../controllers/company.controller');
-// const {
-//   checkUserIdentity,
-// } = require('../middlewares/authentication.middleware');
+const {
+  checkUserIdentity,
+} = require('../middlewares/authentication.middleware');
 const { isLoggedIn } = require('../middlewares/authorization.middleware');
 
 module.exports = router;
 
 // Create a company
-router.post('/', isLoggedIn, company.createCompany);
+router.post('/', isLoggedIn, checkUserIdentity, company.createCompany);
 
 // Delete a company
-router.delete('/', isLoggedIn, company.deleteCompany);
+router.delete('/', isLoggedIn, checkUserIdentity, company.deleteCompany);
 
 // Patch a company
-router.patch('/', isLoggedIn, company.patchCompany);
+router.patch('/', isLoggedIn, checkUserIdentity, company.patchCompany);
