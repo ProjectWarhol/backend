9ccf61f183fefa18e3631692471e0089e750d73f
julklingel <julklingel@gmail.com>
2022-10-21 18:43:02 +0200

    update/seeder

--- db/seeders/3-job.position.js
+++ db/seeders/3-job.position.js
@@ -11,10 +11,14 @@ module.exports = {
       {
         userId: userIds[0][0].id,
         companyId: companyIds[0][1].id,
+        role: 'Software Engineer',
+        department: 'Engineering',
       },
       {
         userId: userIds[0][0].id,
         companyId: companyIds[0][2].id,
+        role: 'CFO',
+        department: 'Finance',
       },
     ]);
   },
