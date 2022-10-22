c9cb7372314e2d2f9b3f0054b2198147e92363b2
julklingel <julklingel@gmail.com>
2022-10-22 20:45:40 +0200

    update/createCompany-controller

--- app/controllers/company.controller.js
+++ app/controllers/company.controller.js
@@ -1,7 +1,55 @@
-// const db = require('../models');
+const db = require('../models');
 
-// const {
-//   Company,
-//   User,
-//   Sequelize: { Op },
-// } = db;
+const {
+  Company,
+  User,
+  Sequelize: { Op },
+} = db;
+
+// Create a company
+exports.createCompany = async (req, res, next) => {
+  const {
+    body: {
+      userId,
+      companyName,
+      website,
+      primaryColor,
+      secondaryColor,
+      address,
+      logo,
+      bio,
+    },
+  } = req;
+
+  User.findById(userId)
+    .then((user) => {
+      user.update({ isCompanyOwner: true });
+      return Company.findOrCreate({
+        where: {
+          [Op.or]: [{ companyName }, { website }],
+        },
+        defaults: {
+          companyName,
+          website,
+          ownerUserId: userId,
+          primaryColor: primaryColor || '#000000',
+          secondaryColor: secondaryColor || '#000000',
+          address: address || '',
+          logo: logo || 'https://pbs.twimg.com/media/FB6YhR8WQAI5MnM.png',
+          bio: bio || '',
+          createdAt: Date.now(),
+        },
+      }).then(([company, created]) => {
+        if (!created) {
+          return res.status(400).send({
+            message: 'Company already exists or user is already owner',
+          });
+        }
+        return res.status(200).send({
+          message: 'Company created successfully',
+          data: company,
+        });
+      });
+    })
+    .catch((err) => next(err));
+};
