37123949a6425641a48c7f78ecadfc843e22ca7b
Massimiliano Ricci <massimiliano.ricci@code.berlin>
2022-10-25 19:09:37 +0200

    merge fucked everything up

--- app/controllers/session.controller.js
+++ app/controllers/session.controller.js
@@ -1,3 +1,21 @@
+exports.login = (req, res, _next) => {
+  return res.status(200).json({
+    message: 'Successfully logged in',
+    user: req.user.stripSensitive(),
+  });
+};
+
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    return res.clearCookie('my.sid', { path: '/' }).status(200).json({
+      message: 'Successfully logged out',
+    });
+  });
+};
+
 // validate existing session from client
 exports.validateSession = (req, res, next) => {
   const { user } = req;
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -13,24 +13,10 @@ const {
 // const { userHasNotWallet } = require('../middlewares/verification.middleware');
 
 // Post login request
-router.post('/login', passport.authenticate('local'), (req, res, _next) =>
-  res.status(200).json({
-    message: 'Successfully logged in',
-    user: req.user.stripSensitive(),
-  })
-);
+router.post('/login', passport.authenticate('local'), session.login);
 
 // Post logout request
-router.post('/logout', isLoggedIn, (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    return res.clearCookie('my.sid', { path: '/' }).status(200).json({
-      message: 'Successfully logged out',
-    });
-  });
-});
+router.post('/logout', isLoggedIn, session.logout);
 
 // Validate session cookie
 router.get('/session', session.validateSession);
