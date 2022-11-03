d52cc023879ec06c8bfd9727af9d988f9dac8457
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-11-03 15:41:34 +0100

    Add config for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API',
+      script: 'npm run dev',
+      time: true,
+      env: {
+        PORT: process.env.PORT,
+        DB_HOST: process.env.DB_HOST,
+        DB_DATABASE: process.env.DB_DATABASE,
+        DB_USERNAME: process.env.DB_USERNAME,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_PORT: process.env.DB_PORT,
+      },
+    },
+  ],
+
+  // deploy: {
+  //   production: {
+  //     user: 'SSH_USERNAME',
+  //     host: 'SSH_HOSTMACHINE',
+  //     ref: 'origin/master',
+  //     repo: 'GIT_REPOSITORY',
+  //     path: 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy':
+  //       'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': '',
+  //   },
+  // },
+};
