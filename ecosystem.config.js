07d21455cba7484bb5254f6ca6888cdd7357a082
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-11-08 15:45:06 +0100

    Create env var on github action

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,15 +18,15 @@ module.exports = {
   deploy: {
     production: {
       user: 'root',
-      host: '46.101.106.146',
-      // key: '~/.ssh/deploy.key',
-      key: process.env.SSH_KEY_PATH_TEST,
+      host: process.env.HOST,
+      key: 'deploy.key',
+      // key: process.env.SSH_KEY_PATH_TEST,
       ref: 'origin/feature/pm2/ci',
       repo: 'https://github.com/ProjectWarhol/backend.git',
       path: '/var/www/production',
       env: {
         IP_ADDRESS: process.env.IP_ADDRESS,
-        NODE_ENV: 'production',
+        NODE_ENV: process.env.NODE_ENV,
         FOO_COOKIE_SECRET: process.env.FOO_COOKIE_SECRET,
         SEQUELIZE_LOGGING: process.env.SEQUELIZE_LOGGING,
         DB_URL: process.env.DB_URL,
